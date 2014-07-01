15a5007312c6c9a22ce32c1667005549f3eaab45
Afnan Fahim <afahim@qatar.cmu.edu>
2014-07-01 20:28:01 +0500

    checking easing

--- js/que.js
+++ js/que.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     $(".list-group-item").click(function() {
         $(".list-group-item").removeClass("active");
-        $(this).addClass("active");
+        $(this).addClass("active", 2000, "easeInBack");
     });
 }
