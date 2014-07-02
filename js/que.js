4dbdcfe9e22e559165c491b92e70b7f1c2b04d46
Afnan Fahim <afahim@qatar.cmu.edu>
2014-07-03 04:56:20 +0500

    finalized; enqueue function works

--- js/que.js
+++ js/que.js
@@ -1,19 +1,7 @@
 window.onload = function() {
-    $(".list-group-item").click(function() {
-        if (!$(this).hasClass('active')) {
-            $(".list-group-item").removeClass("active");
-            $(".panel-body").slideUp();
-            $(this).addClass("active");
-            $(this).next().slideDown(400);            
-        } else {
-            $(this).removeClass("active");
-            $(this).next().slideUp(400);            
-        }
-    });
+    $(".list-group-item").click(queueItemHandler);
 
-    $(".glyphicon-ok").click(function() {
-       $(this).css("color", "#00D700");
-    });
+    $(".glyphicon-ok").click(glyphOKHandler);
 
     $(".glyphicon-plus").click(function() {
         if ($(this).hasClass('rotated')){
@@ -26,12 +14,65 @@ window.onload = function() {
     });
 
     $(".btn").click(function() {
-        var name = $("#name").text();
-        var number = $("#number").text();
+        var name = document.getElementById('name').value;
+        var number = document.getElementById('number').value;
 
         $(".glyphicon-plus").removeClass('rotated');
         $("#enqueue-panel").slideUp(400);
 
+        var count = document.getElementsByClassName('list-group-item').length + 1;
+
+        var node=document.createElement("a");
+        node.href = "#";
+        node.className = "list-group-item";
+
+        var icon=document.createElement("span");
+        icon.className = "glyphicon glyphicon-ok"
+        node.appendChild(icon);
+
+        $(icon).click(glyphOKHandler);
+
+        var textnode=document.createTextNode(name);
+        node.appendChild(textnode);
+
+        var badge=document.createElement("span");
+        badge.className = "badge"
+
+        var badgeText = document.createTextNode(count);
+        badge.appendChild(badgeText);
+
+        node.appendChild(badge);
+
+        $(node).click(queueItemHandler);
+
+        document.getElementById("queue").appendChild(node);
+
+        var numNode=document.createElement("div");
+        numNode.className = "panel-body";
+
+        var textnode=document.createTextNode(number);
+        numNode.appendChild(textnode);
+
+        document.getElementById("queue").appendChild(numNode);
+
+        document.getElementById('name').value = "";
+        document.getElementById('number').value = "";
     });
 
+}
+
+function queueItemHandler() {
+        if (!$(this).hasClass('active')) {
+            $(".list-group-item").removeClass("active");
+            $(".panel-body").slideUp();
+            $(this).addClass("active");
+            $(this).next().slideDown(400);            
+        } else {
+            $(this).removeClass("active");
+            $(this).next().slideUp(400);            
+        }
+}
+
+function glyphOKHandler() {
+        $(this).css("color", "#00D700");   
 }
