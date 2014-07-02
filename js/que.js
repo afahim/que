92bc588c9208faa3c49689c56d9e9c21d1ed7015
Afnan Fahim <afahim@qatar.cmu.edu>
2014-07-03 00:38:27 +0500

    insertion form

--- js/que.js
+++ js/que.js
@@ -1,13 +1,19 @@
 window.onload = function() {
 
     $(".list-group-item").click(function() {
-        $(".list-group-item").removeClass("active");
-        $(".panel-body").slideUp();
-        $(this).addClass("active");
-        $(this).next().slideDown(400);
+        if (!$(this).hasClass('active')) {
+            $(".list-group-item").removeClass("active");
+            $(".panel-body").slideUp();
+            $(this).addClass("active");
+            $(this).next().slideDown(400);            
+        }
     });
 
     $(".glyphicon-ok").click(function() {
        $(this).css("color", "#00D700");
+    });
+
+    $(".glyphicon-plus").click(function() {
+        $("#enqueue-panel").slideDown(400);
     });
 }
